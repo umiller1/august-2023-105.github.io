7778fcdde3864af56f813e14c8188473f51c133e
Unique <unique.miller@first.edu>
2024-01-22 11:38:12 -0500

    Lab day setup

--- agenda/main.js
+++ agenda/main.js
@@ -2,3 +2,16 @@ $('.timeline li').on('click', function(){
     $('.timeline li.activo').removeClass('activo');
     $(this).addClass('activo');
 });
+
+
+// give cards random colors on load
+$(document).ready(function(){
+
+    var colors = ["#FFD700","#00E1FF","#FF33AE","#00E2BF","#FFC3AD","#F2FEDC"];
+    
+    $('.card').each(function(){
+      var new_color = colors[Math.floor(Math.random()*colors.length)];
+      $(this).css('background-color',new_color);
+    });
+   
+  });
